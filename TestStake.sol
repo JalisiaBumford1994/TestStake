ea4bb26c661d91c18c8922d26f0b3d2fbda2d111
JalisiaBumford1994 <101624432+JalisiaBumford1994@users.noreply.github.com>
2023-11-21 07:52:24 +0200

    Update TestStake.sol

--- TestStake.sol
+++ TestStake.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StakingPool {
+    address public owner;
+    IERC20 public stakingToken;
+    uint256 public totalStaked;
+    mapping(address => uint256) public stakedBalances;
+    mapping(address => uint256) public lastStakeTime;
+
+    event Staked(address indexed staker, uint256 amount);
+    event Unstaked(address indexed staker, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    constructor(address _stakingToken) {
+        owner = msg.sender;
+        stakingToken = IERC20(_stakingToken);
+    }
+
+
+}
